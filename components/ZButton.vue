dfcfad6fb68c1ea9043dcf179ffd41bbd3d6bc23
Samuel BOUAROUA <samuel.bouaroua@jellysmack.com>
2022-08-07 18:36:05 +0200

    feat: product list

---
+++ components/ZButton.vue
@@ -0,0 +1,76 @@
+<template>
+  <component :is="as" :class="bindings" v-on="$listeners">
+    <slot />
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'ZButton',
+  props: {
+    as: {
+      type: String,
+      default: 'button',
+      validator: (value) => ['button', 'a', 'nuxt-link'].includes(value),
+    },
+    variant: {
+      type: String,
+      default: 'regular',
+      validator: (value) => ['regular', 'outline'].includes(value),
+    },
+    size: {
+      type: String,
+      default: 'md',
+      validator: (value) => ['sm', 'md', 'lg'].includes(value),
+    },
+  },
+  computed: {
+    bindings() {
+      const { size, variant } = this.$props
+      return [this.$style.zButton, this.$style[size], this.$style[variant]]
+    },
+  },
+}
+</script>
+
+<style lang="scss" module>
+.zButton {
+  cursor: pointer;
+  font-weight: 300;
+  text-transform: uppercase;
+  transition: all 0.3s ease-in-out;
+
+  &.xs {
+    padding: 4px;
+  }
+  &.sm {
+    padding: 8px;
+  }
+  &.md {
+    padding: 12px;
+  }
+  &.lg {
+    padding: 16px;
+  }
+
+  &.regular {
+    color: white;
+    background-color: black;
+    border: 1px solid black;
+
+    &:hover {
+      background-color: #232323;
+    }
+  }
+
+  &.outline {
+    border: 1px solid black;
+    background-color: transparent;
+
+    &:hover {
+      background-color: black;
+      color: white;
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,38 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        Nuxt
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <ul :class="$style.productList">
+      <li v-for="product in $store.state.products.items" :key="product.id">
+        {{ product.title }}
+        <ZButton @click="addToCart(product)">+ Add to cart</ZButton>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'IndexPage',
+  computed: {
+    products() {
+      return this.$store.state.products.items
+    },
+  },
+  methods: {
+    addToCart(product) {
+      this.$store.dispatch('cart/CREATE_ITEM', product)
+    },
+  },
+}
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+<style lang="scss" module>
+.productList {
+  max-width: 30vw;
+  li {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 1rem;
+  }
 }
 </style>
